5bf6dd4f08d301d464680d77726d00bda77c0072
xSarscov <vargasernest7@gmail.com>
2023-09-16 19:50:11 -0600

    agregando los últimos cambios

--- js/main.js
+++ js/main.js
@@ -64,3 +64,40 @@
     
 })(jQuery);
 
+// Sending mails with EmailJS
+
+const ApiKey = "o5K1B5X-f-DV-Hbyy";
+const ServiceID = "service_x4u364c";
+const TemplateID = "template_bmu10d4";
+
+const Name = document.querySelector("#name");
+const Subject = document.querySelector("#subject");
+const Email = document.querySelector("#email");
+const Message = document.querySelector("#message");
+const SubmitBtn = document.querySelector("#submit-btn");
+const Form = document.querySelector("#form");
+
+emailjs.init(ApiKey);
+
+Form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    SubmitBtn.innerText = "Espere un momento...";
+
+    const inputfields = {
+        name: Name.value,
+        subject: subject.value,
+        email: Email.value,
+        message: Message.value
+    };
+
+    emailjs.send(ServiceID, TemplateID, inputfields)
+    .then(() => {
+        SubmitBtn.innerText = "¡Mensaje enviado correctamente!";
+        Form.reset();
+    }, (error) => {
+        console.log(error);
+        SubmitBtn.innerText = "¡Algo salió mal!";
+    });
+});
+
