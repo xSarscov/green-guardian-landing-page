2cd491689f4184c5adf45019422b9702e21deae6
xSarscov <vargasernest7@gmail.com>
2023-09-24 15:22:47 -0600

    modificando contacto y descarga

--- js/main.js
+++ js/main.js
@@ -77,13 +77,18 @@
         return false;
     });
 
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
+    // Modal Video
+    var $videoSrc;
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
     });
-
+    console.log($videoSrc);
+    $('#videoModal').on('shown.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    $('#videoModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc);
+    })
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
